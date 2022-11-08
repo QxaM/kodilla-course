81e2da656aaff2587b267f8e6e2920ab2563f28a
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2022-11-08 20:54:18 +0100

    6.2 - Added Calculator class and tests for add and substract method

--- kodilla-testing/src/main/java/com/kodilla/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6 - wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int resultCalc = calculator.add(2,7);
+        System.out.println("Testing - first unit test for class Calculator: ");
+        if(resultCalc == (2+7)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        resultCalc = calculator.substract(2, 7);
+        System.out.println("Testing - second unit test for class Calculator: ");
+        if(resultCalc == (2-7)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
